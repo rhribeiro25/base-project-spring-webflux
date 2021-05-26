6393d2abf98ae6a31b94df3b67000d921470f3e0
rhribeiro25 <renan.ribeiro@dasa.com.br>
2021-05-25 21:36:46 -0300

    feat: Exams OK

---
+++ src/test/java/br/com/rhribeiro25/baseprojectspringwebflux/BaseProjectSpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rhribeiro25.baseprojectspringwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectSpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
