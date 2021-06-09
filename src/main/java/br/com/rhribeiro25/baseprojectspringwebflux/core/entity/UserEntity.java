b393a8935fca1030b7829dd549b93e6eaad76d54
rhribeiro25 <renan.ribeiro@dasa.com.br>
2021-06-08 22:06:51 -0300

    feat: R2DBC configurations OK

--- src/main/java/br/com/rhribeiro25/baseprojectspringwebflux/core/entity/UserEntity.java
+++ src/main/java/br/com/rhribeiro25/baseprojectspringwebflux/core/entity/UserEntity.java
@@ -4,7 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Column;
 
+import java.util.Date;
 import java.util.Set;
 
 /**
@@ -18,13 +23,35 @@ import java.util.Set;
 @NoArgsConstructor
 @Builder
 public class UserEntity {
+    @Id
     private Long id;
+
+    @Column
     private String firstName;
+
+    @Column
     private String middleName;
+
+    @Column
     private String lastName;
-    private RoleEntity role;
+
+    @Column
     private String email;
+
+    @Column
     private String password;
+
+    @Column
+    @CreatedDate
+    private Date createdAt;
+
+    @Column
+    @LastModifiedDate
+    private Date updatedAt;
+
+    private RoleEntity role;
+
     private Set<PhoneEntity> phones;
+
     private Set<CompanyEntity> companies;
 }
