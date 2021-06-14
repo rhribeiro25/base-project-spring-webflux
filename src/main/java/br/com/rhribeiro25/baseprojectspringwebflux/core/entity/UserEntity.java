2d22604639ab304f60b8f55ef1c73864cd5d1dba
rhribeiro25 <rhribeiro_25@hotmail.com>
2021-06-13 23:19:16 -0300

    feat: Migrations OK

--- src/main/java/br/com/rhribeiro25/baseprojectspringwebflux/core/entity/UserEntity.java
+++ src/main/java/br/com/rhribeiro25/baseprojectspringwebflux/core/entity/UserEntity.java
@@ -4,7 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
 
 /**
  * Class User Entity
@@ -27,4 +32,8 @@ public class UserEntity {
     private String password;
     private String phone;
     private String role;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
